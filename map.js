8a87f31a3728af1e653a070327742b5d6f5eeb01
nathphan <nathphan510@gmail.com>
2017-01-21 01:13:50 -0800

    Separated HTML and JS files

---
+++ map.js
@@ -0,0 +1,19 @@
+      var map;
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map,
+          draggable:true,
+          title:"Drag Me!"
+        });
+      }
+      
+      $('#btnaddmarker').on('click', function(){
+        addmarker(IT)
+      })
+
